9e52c3362661e88733075618d80f6d0902d4bc27
alester77 <113203488+alester77@users.noreply.github.com>
2022-12-09 00:53:08 -0700

    adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,75 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require ('fs');
+const { title } = require('process');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your Project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please describe your project.',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Please give installation instructions for your project.',
+  },
+  {
+    type: 'input',
+    name: 'instruction',
+    message: 'Please give instruction of use for your project.',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please give the usage for your project.',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Please give contribution credits for your project.',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'Please give tests for your project.',
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'Please select a license.',
+    choices: ['MIT', 'Apache', 'GNU'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your github URL?',
+  },
+
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+  },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  
+};
 
 // Function call to initialize app
 init();
